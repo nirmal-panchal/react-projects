d2deb2d4281a0dca957e79a8d9883e7bb582783f
nirmal-panchal <nirmalpanchal85@gmail.com>
2023-08-29 09:36:16 +0530

    first commit

---
+++ brand-page/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <Navbar />
+        <Hero />
+      </div>
+    </>
+  );
+}
+
+export default App;
